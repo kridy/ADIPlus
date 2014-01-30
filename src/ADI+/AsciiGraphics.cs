f52dccbd6d41a167ce79510b8613117970f463e2
Kristian Dyhrberg <krd@kamstrup.dk>
2014-01-30 16:31:31 +0100

    some minor detail

--- src/ADI+/AsciiGraphics.cs
+++ src/ADI+/AsciiGraphics.cs
@@ -1,6 +1,29 @@
-﻿namespace ADIPlus
+﻿using System;
+
+namespace ADIPlus
 {
     public class AsciiGraphics
     {
+        public void SetChar(int x, int y, AsciiColor ch)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.BackgroundColor = ch.BackgroundColor;
+            Console.ForegroundColor = ch.ForgroundColor;
+            Console.Write(ch.Character);
+        }
+    }
+
+    public  class AsciiColor
+    {
+        public AsciiColor(ConsoleColor forgroundColor, ConsoleColor backgroundColor, char character)
+        {
+            ForgroundColor = forgroundColor;
+            BackgroundColor = backgroundColor;
+            Character = character;
+        }
+
+        public ConsoleColor ForgroundColor { get; set; }
+        public ConsoleColor BackgroundColor { get; set; }
+        public char Character { get; set; }
     }
 }
--- src/ADI+/AsciiLabel.cs
+++ src/ADI+/AsciiLabel.cs
@@ -1,5 +1,18 @@
-﻿namespace ADIPlus
+﻿using System;
+
+namespace ADIPlus
 {
+    public class AsciiChar : AsciiControle
+    {
+        public override void OnPaint(AsciiPaintEventArgs e)
+        {
+            var g = e.Graphics;
+            g.SetChar(0,0,new AsciiColor(ConsoleColor.Blue, ConsoleColor.Yellow, '*'));
+        }
+
+        public string Text { get; set; }
+    }
+
     public class AsciiLabel : AsciiControle
     {
         public override void OnPaint(AsciiPaintEventArgs e)
